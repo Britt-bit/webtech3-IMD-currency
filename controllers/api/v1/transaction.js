384cbf4cc3e9b14fc048b92e6413aab2b293573d
britt <britt110100@gmail.com>
2020-05-07 19:26:50 +0200

    oplossing vinden voor fromuser en touser

--- controllers/api/v1/transaction.js
+++ controllers/api/v1/transaction.js
@@ -1,26 +1,21 @@
 const mongoose = require('mongoose');
-//const user = require('./user_data');
+const User = require('./user_data');
 //console.log(userSchema.name);
 const Schema = mongoose.Schema;
 const transactionSchema = new Schema({
     //transaction_id: ObjectId,
     //user: [{id: ObjectId, name: String, lastname: String, email: String, password: String, coins: Number}],
-    fromUser: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    toUser: {
-        type: Schema.ObjectId,
-        ref: 'User'
-    },
+    //fromUser: {type: String,ref: 'User'},
+    //toUser: {type: Schema.ObjectId,ref: 'User'},
     coins: {
         type: Number,
         required: true
     },
     date: {type: Date, default: Date.now},
-    reason: String,
-    message: String,
-    completed: Boolean
+    reason: {
+        type: String},
+    message: {type: String},
+    completed: {type: Boolean}
 });
 const Transaction = mongoose.model('Transaction', transactionSchema);
 //const User = mongoose.model('User', userSchema);
@@ -39,22 +34,16 @@ const getAll = (req, res) => {
 
 const create = (req, res) => {
     let transaction = new Transaction();
-    transaction.fromUser = new mongoose.Types.ObjectId;;
-    transaction.toUser = new mongoose.Types.ObjectId;;
+    let message = req.body.message;
+    //transaction.fromUser = new mongoose.Types.ObjectId;;
+    //transaction.toUser = new mongoose.Types.ObjectId;;
     transaction.coins = 20;
 
     transaction.reason = "Help with developing";
-    transaction.message = "Thank you";
+    transaction.message = message;
     transaction.completed = false;
 
-    user.save((err, doc) => {
-        if(!err){
-            res.json({
-                "status": "success",
-                "data": doc
-            });
-        }
-    })
+
     transaction.save((err, doc) => {
         if(!err){
             res.json({
