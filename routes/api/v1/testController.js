74204bb4e9e69b96b1110e62ceae6968b094566e
britt <britt110100@gmail.com>
2020-05-20 19:11:43 +0200

    trying to fix cors error

--- routes/api/v1/testController.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const testController = require("../../../controllers/api/v1/testController");
-
-router.get("/:id", testController.getById);
-router.post("/", testController.create);
-module.exports = router;
