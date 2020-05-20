8c6f66c0d5875d2615c65200211b28f6939b584c
Baribal172 <vincentvandesompel@hotmail.com>
2020-05-20 16:27:58 +0200

    Mongoose update + Added Create to test, getbyId works

--- controllers/api/v1/testController.js
+++ controllers/api/v1/testController.js
@@ -1,25 +1,42 @@
 const Transaction = require("../../../models/Transaction");
 
 const getById = (req, res) => {
-	Transaction.find({"_id": req.params.id}, (err, docs) => {
+	Transaction.find({ _id: req.params.id }, (err, docs) => {
 		console.log(req);
-			if (!err) {
-				res.json({
-					"status": "success",
-					"data": {
-						"data": docs
-					}
-				});
-			}
-			if (err) {
-				res.json({
-					status: "error",
-					data: "Could not find a message with id: " + req.params.id,
-				});
-			}
+		if (!err) {
+			res.json({
+				status: "success",
+				data: {
+					data: docs,
+				},
+			});
 		}
-	);
+		if (err) {
+			res.json({
+				status: "error",
+				data: "Could not find a message with id: " + req.params.id,
+			});
+		}
+	});
 };
-
-
+const create = (req, res) => {
+	let transaction = new Transaction();
+	transaction.fromUser = "1221313";
+	transaction.toUser = "1231";
+	transaction.coins = "1";
+	transaction.reason = "wee";
+	transaction.message = "bro";
+	transaction.completed = false;
+	transaction.save((err, doc) => {
+		if (!err) {
+			res.json({
+				status: "success",
+				data: {
+					transaction: doc,
+				},
+			});
+		}
+	});
+};
 module.exports.getById = getById;
+module.exports.create = create;
--- routes/api/v1/testController.js
+++ routes/api/v1/testController.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const testController = require("../../../controllers/api/v1/testController");
 
 router.get("/:id", testController.getById);
-
+router.post("/", testController.create);
 module.exports = router;
