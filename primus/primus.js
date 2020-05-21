4c54715e55802b7dc35f515676013a3656b2e6c3
britt <britt110100@gmail.com>
2020-05-21 17:24:05 +0200

    added spark.on listen

--- primus/primus.js
+++ primus/primus.js
@@ -4,6 +4,11 @@ let go = (server) => {
     let primus = new Primus(server, {});
     primus.on('connection', (spark) => {
         console.log('🔥 received spark 🔥');
+
+        spark.on('data', (data) => {
+            console.log(data);
+            console.log("you're doing great");
+        })
     });
 
 }
